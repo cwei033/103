d945bb36e026bbe540c81023595c2a50eb075435
Julie <60452220+cwei033@users.noreply.github.com>
2021-02-01 02:22:43 -0800

    icon

--- abandonnormalinstruments/sketch.js
+++ abandonnormalinstruments/sketch.js
@@ -2,9 +2,13 @@ var song;
 var amp;
 var fft;
 var volhistory = [];
+var img;
+let iconRotate = 0;
+let rSpeed = 0.1;
 
 function preload() {
-  song = loadSound('103obliquestrategies.mp3')
+  song = loadSound('103obliquestrategies.mp3');
+  img = loadImage('icon.png');
 }
 
 function setup() {
@@ -14,6 +18,7 @@ function setup() {
   angleMode(DEGREES);
   rectMode(CENTER);
   textAlign(CENTER);
+  imageMode(CENTER);
 
   amp = new p5.Amplitude();
   song.play();
@@ -25,10 +30,18 @@ function draw() {
   background(0);
   var vol = amp.getLevel();
   volhistory.push(vol);
-  fill(255);
-  textFont('monospace');
-  textSize(width*.01);
-  text('abandon normal instruments', width/2, height/2);
+  // fill(255);
+  // textFont('monospace');
+  // textSize(width*.01);
+  // text('abandon normal instruments', width/2, height/2);
+
+  drawIcon(iconRotate);
+  iconRotate += rSpeed;
+  if (iconRotate < -5 || iconRotate > 5) {
+    rSpeed = -rSpeed;
+  }
+
+
   noFill();
   stroke(255);
   translate(width / 2, height / 2);
@@ -46,4 +59,13 @@ function draw() {
   if (volhistory.length > 360) {
     volhistory.splice(0, 1);
   }
+}
+
+function drawIcon(rotation) {
+  push();
+  translate(width / 2, height / 2);
+  rotate(rotation);
+  img.resize(width*.2, 0);
+  image(img, 0, 0);
+  pop();
 }
